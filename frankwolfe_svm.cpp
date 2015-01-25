635a72e5312e8e98b2606c9a9d2b8a71814c50fb
Ali <me@alirezabagheri.com>
2015-01-24 20:06:51 -0800

    Updating README.md

--- frankwolfe_svm.cpp
+++ frankwolfe_svm.cpp
@@ -17,7 +17,7 @@
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
-	// Input data is in sparse format (see matlab function 'splitAtomMatrix.m').
+	// Input data is in sparse format (see matlab function 'prepare_data.m').
 	// NOTE: first ROW of input is label.
 
 */
